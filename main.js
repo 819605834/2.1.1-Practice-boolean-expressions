60ddecf74bc23d47d0fdbb895216b7b27a8e8162
819605834 <124411012+819605834@users.noreply.github.com>
2026-05-26 19:29:26 +0000

    Practice boolean expressions

--- main.js
+++ main.js
@@ -2,21 +2,21 @@
 // evaluate to true. Change just the value on the
 // left side of the expression to make it evaluate
 // to false instead.
-console.log(101 > 6);
+console.log(1.01 > 6);
 
-console.log(15 === 60 / 4);
+console.log('15' === 60 / 4);
 
-console.log("Hello" !== "hello");
+console.log("hello" !== "hello");
 
 // The next three expressions all currently
 // evaluate to false. This time change the
 // operator (not the values themselves!) to
 // make them evaluate to true.
-console.log(6 < 6);
+console.log(6 <= 6);
 
-console.log("A" > "a");
+console.log("A" != "a");
 
-console.log(false === true);
+console.log(false != true);
 
 // Now write three of your own boolean
 // expressions from scratch. They can be using
